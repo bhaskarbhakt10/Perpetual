112618e5a3daa29416b0bbc42b7d841f90b0e4e3
bhaskar <bhaskar@nuitsolutions.com>
2024-01-12 20:14:43 +0530

    form

---
+++ register/pages/register.php
@@ -0,0 +1,57 @@
+<div class="container">
+
+    <form action="" method="post">
+
+        <div class="row">
+
+            <div class="col-md-6">
+
+                <div class="mb-3 field-parent">
+                    <label for="firstname"> First Name <span>*</span></label>
+                    <input type="text" name="firstname" id="firstname" class="form-control" required>
+                </div>
+
+            </div>
+
+            <div class="col-md-6">
+
+                <div class="mb-3 field-parent">
+                    <label for="lastname">Last Name <span>*</span></label>
+                    <input type="text" name="lastname" id="lastname" class="form-control" required>
+                </div>
+
+            </div>
+
+            <div class="col-md-6">
+                <div class="mb-3 field-parent">
+                    <label for="country_of_residence"> Country Of Residence <span>*</span> </label>
+                    <input type="text" name="" id="country_of_residence" class="form-control" readonly>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="mb-3 field-parent">
+                    <label for="phone_number">Phone Number <span>*</span></label>
+                    <input type="tel" name="phone_number" id="phone_number" class="form-control" required>
+                </div>
+            </div>
+
+
+            <div class="col-md-6">
+                <div class="mb-3 field-parent">
+                    <label for="email">Email <span>*</span></label>
+                    <input type="email" name="email" id="email" class="form-control" required>
+                </div>
+            </div>
+
+            <div class="">
+
+            </div>
+
+        </div>
+
+
+    </form>
+
+
+</div>
